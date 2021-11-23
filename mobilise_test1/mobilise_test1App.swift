85d7d5f94a8df8242785219d39e53289aa2d09fc
Georgii Polonskii <gopolons@gmail.com>
2021-11-23 19:48:49 +0000

    Initial Commit

---
+++ mobilise_test1/mobilise_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  mobilise_test1App.swift
+//  mobilise_test1
+//
+//  Created by Georgy Polonskiy on 23/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct mobilise_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
